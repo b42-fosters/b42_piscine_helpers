f632dbc644c0cc396e84c0de5eab13820c3a505c
Fedor Dikarev <fedor.dikarev@gmail.com>
2024-03-13 15:37:53 +0100

    add summary status and filename to function name check

--- b42_prepush_check.sh
+++ b42_prepush_check.sh
@@ -2,14 +2,18 @@
 
 git_root=$(git rev-parse --show-toplevel)
 
+summary_status=""
+
 pushd "$git_root"
 echo "== NORM"
 norminette
 norminette_status=$?
+summary_status="${summary_status}.${norminette_status}"
 
 echo "== CC"
 cc -Wall -Wextra -Werror -c */*.c
 cc_status=$?
+summary_status="${summary_status}.${cc_status}"
 
 echo "== Files in repo"
 git ls-files
@@ -18,11 +22,25 @@ echo $?
 echo "== Git uncommited"
 git status --short --untracked-files=no
 uncommited_status=$(git status --short --untracked-files=no | wc -l)
+summary_status="${summary_status}.${uncommited_status}"
 
+echo "== Filename to function name"
+not_found=0
+for fname in $(find . -name 'ft_*.c'); do
+	func_name=$(basename -s .c $fname);
+	# grep "^/.*$func_name" "$fname"	# Check for fname in header
+	egrep -H "^(char|int|void)\s+\*?$func_name\(" $fname # Check for the function declaration
+	not_found=$((not_found + $?))
+done
+summary_status="${summary_status}.${not_found}"
+
+summary_status="${summary_status}."
+summary_status_filtered=$(echo "$summary_status" | tr -d.0)
 RED='\033[0;91m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-if [ "$norminette_status" -ne 0 -o "$cc_status" -ne 0 -o "$uncommited_status" -ne 0 ];
+echo "Summary code: ${summary_status}"
+if [ "$summary_status_filtered" != "" ];
 then
 	echo -e "$RED## SOME STEPS CAN BE IMPROVED."
 	echo -e "Do you mind to take a deeper look there?$NC"
